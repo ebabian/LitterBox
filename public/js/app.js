1ed6ae4b10cd6fd78ada867cd5b607218c8d5081
Elektra Babian <babianelektra@gmail.com>
2020-06-17 13:12:59 -0400

    structured app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,26 @@
 console.log("I work!")
+
+const app = angular.module('LitterApp', [])
+
+app.controller('CatController', ['$http',
+function($http){
+  this.username = '';
+  this.entry= '';
+  this.date = 0;
+  this.likes = 0;
+
+
+// get function
+
+
+// create 'post'
+
+
+//edit 'put'
+
+
+//delete
+
+
+//call get function on page load
+}])
