91a1b3f71c1fcebb11bf897ced2d0e7bcd04275e
David Lance <david6116@yahoo.com>
2020-06-17 12:59:16 -0400

    add controller code

--- controllers/litterBoxRoute.js
+++ controllers/litterBoxRoute.js
@@ -4,22 +4,36 @@ const Litter = require('../models/litter.js');
 
 //Create a post
 router.post('/', (req, res) => {
-
+    Litter.create(req.body, (err, createdLitter) => {
+        res.json(createdLitter);
+    })
 });
 
 //Delete a post
 router.delete('/:id', (req, res) => {
+    Litter.findByIdAndRemove(req.params.id, (err, deletedLitter) => {
+        res.json(deletedLitter);
+    });
 
 })
 
 //Update a post
 router.put('/:id', (req, res) => {
-
+    Litter.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true },
+        (err, updatedLitter) => {
+            res.json(updatedLitter);
+        }
+    )
 })
 
 //Get a list of all posts
 router.get('/', (req, res) => {
-
+    Litter.find({}, (err, foundLitter) => {
+        res.json(foundLitter);
+    })
 });
 
 module.exports = router;
